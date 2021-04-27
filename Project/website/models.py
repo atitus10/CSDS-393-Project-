688e8330d1155d8f6365084bc0f1abfe4619e921
Olivia Weng <osw9@case.edu>
2021-04-27 04:07:11 -0400

    login

--- Project/website/models.py
+++ Project/website/models.py
@@ -8,4 +8,12 @@ class MenuItem(models.Model):
     def __str__(self):
         return self.question_text
 
-# Create your models here.
+class RestaurantUsers(models.Model):
+    menu_item_name = models.CharField(max_length=200)
+    menu_item_description = models.CharField(max_length=200)
+    menu_item_price = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.question_text
+        
+# Create your models here
